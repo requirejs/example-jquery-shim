b60bdb11930037063f6276d76124bd1f790fa8d4
cmaj135 <cmaj135@hotmail.com>
2014-02-21 10:12:02 +0800

    minor fix for visit directory

--- tools/server.js
+++ tools/server.js
@@ -18,20 +18,31 @@ http.createServer(function (request, response) {
     var uri = url.parse(request.url).pathname,
         filename = path.join(__dirname, '..', uri);
 
-    fs.exists(filename, function (exists) {
-        if (!exists) {
-            response.writeHead(404, {'Content-Type': 'text/plain'});
-            response.write('404 Not Found\n');
-            response.end();
-            return;
-        }
+    if(!fs.lstatSync(filename).isDirectory()) {
+        fs.exists(filename, function (exists) {
+            if (!exists) {
+                response.writeHead(404, {'Content-Type': 'text/plain'});
+                response.write('404 Not Found\n');
+                response.end();
+                return;
+            }
 
-        var type = filename.split('.');
-        type = type[type.length - 1];
+            var type = filename.split('.');
+            type = type[type.length - 1];
 
-        response.writeHead(200, { 'Content-Type': types[type] + '; charset=utf-8' });
-        fs.createReadStream(filename).pipe(response);
-    });
+            response.writeHead(200, { 'Content-Type': types[type] + '; charset=utf-8' });
+            fs.createReadStream(filename).pipe(response);
+        });
+    } else {
+        /**
+         * if users visit the site such as http://localhost:8888
+         * then lead them to http://localhost:8888/www/app.html
+         *
+         */
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write('Please visit <a href="www/app.html">here</a>\n');
+        response.end();
+    }
 }).listen(parseInt(port, 10));
 
 console.log('Static file server running at\n  => http://localhost:' + port + '/\nCTRL + C to shutdown');
