2f6812334df49c418b28e0791915266e2c31a7fd
Karl Westin <karl.westin@gmail.com>
2014-02-24 19:35:38 -0800

    get rid of my comments (sorry james, i didn't get so much sleep lately)

--- tools/server.js
+++ tools/server.js
@@ -18,12 +18,6 @@ var http = require('http'),
 http.createServer(function (request, response) {
     var uri = url.parse(request.url).pathname,
         filename = path.join(__dirname, '..', uri);
-/*
-    if(/favicon.ico$/.test(uri)) {
-      response.writeHead(204, {});
-      return response.end();
-    }
-    */
 
     fs.exists(filename, function (exists) {
       if (!exists) {
